3395acb68641589ba3b5781fa8201b8cb3bf8d7b
Arnaud Deprez <arnaud.deprez@lampiris.be>
2015-01-22 11:43:10 +0100

    use the new @SpringBootApplication annotation

--- rest/account-spring-boot/src/main/java/be/arndep/camel/rest/Application.java
+++ rest/account-spring-boot/src/main/java/be/arndep/camel/rest/Application.java
@@ -2,15 +2,14 @@ package be.arndep.camel.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by arnaud on 30.12.14.
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
     
     public static void main(String[] args) {
